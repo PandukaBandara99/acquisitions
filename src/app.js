0e484a7d8f85388eea859b2974765ea4ee8f19fe
pandukabandara99 <e19039@eng.pdn.ac.lk>
2025-12-15 15:26:50 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+// Setup express application with right middleware and port configuration
+import e from 'express';
+import express from 'express';
+
+const app = express();
+
+// 
+app.get('/', (req, res) => {
+  res.status(200).send('Hello from the acquisitions service!');
+});
+
+//export the app instance for use in other modules
+export default app;
